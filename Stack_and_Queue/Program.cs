da798b205c2d222c4c5f59c70555db0fcff9ffeb
Tushar Anand <tusharranand@gmail.com>
2022-03-28 16:04:45 +0530

    [Tushar Anand] Added: Ability to create a stack

--- Stack_and_Queue/Program.cs
+++ Stack_and_Queue/Program.cs
@@ -11,6 +11,16 @@ namespace Stack_and_Queue
         public static void Main(string[] args)
         {
             Console.WriteLine("Stack and Queue using Linked List");
+
+            Node node_one = new Node(56);
+            Node node_two = new Node(30);
+            Node node_three = new Node(70);
+
+            Stack stack = new Stack();
+            stack.Push(node_one);
+            stack.Push(node_two);
+            stack.Push(node_three);
+            stack.Display();
         }
     }
 }
---
+++ Stack_and_Queue/Stack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue
+{
+    public class Stack
+    {
+        Linked_List linked_list;
+
+        public Stack()
+        {
+            linked_list = new Linked_List();
+        }
+        public void Push(Node node)
+        {
+            linked_list.AddNode(node);
+        }
+        public void Display()
+        {
+            linked_list.Display();
+        }
+    }
+}
